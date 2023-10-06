0dc258384f3a1233ee984e8d5c65c10b2dd4928a
felix <felix.weglehner@gmx.de>
2023-10-06 16:48:42 +0200

    Updated some stuff

--- js/app.js
+++ js/app.js
@@ -1,31 +1,19 @@
 const peersControlsElement = document.getElementById("peers-controls-container");
 const peersElement = document.getElementById("peers-container")
 const clientStreamElement = document.getElementById("client-stream");
+const settingsContainerElement = document.getElementById("settings-container");
 
 const peers = [{"uuid": "12345", "name":"Felix"}, {"uuid": "123456", "name":"Paula"}];
 
 peers.forEach((p) => {
-    const peerControlElement = createPeerControlElement(p.name, 
-        () => toggleStreamingButton(peerControlElement), 
-        () => toggleStreamingButton(peerControlElement))
+    const peerControlElement = createPeerControlElement(p.name, () => {}, () => {});
     p.peerControlElement = peerControlElement;
     peersControlsElement.appendChild(peerControlElement);
+    const peerElement = createPeerElement(p.name);
+    p.peerElement = peerElement;
+    peersElement.appendChild(peerElement);
 })
 
-function toggleStreamingButton(peerControlElement) {
-    const startBtn = peerControlElement.getElementsByClassName("pc-peer-start-btn")[0];
-    const stopBtn = peerControlElement.getElementsByClassName("pc-peer-stop-btn")[0];
-    if (startBtn.classList.contains("hidden")) {
-        startBtn.classList.remove("hidden");
-        stopBtn.classList.add("hidden");
-    } else {
-        startBtn.classList.add("hidden");
-        stopBtn.classList.remove("hidden");
-    }
+function toggleSettings() {
+    settingsContainerElement.classList.toggle("hidden");
 }
-
-//const socket = new WebSocket("wss://stream.codelix.de/wss");
-
-/*socket.onmessage = (data) => {
-
-}*/
--- js/elements.js
+++ js/elements.js
@@ -6,8 +6,10 @@ function createPeerControlElement(name, onStart, onStop) {
     const nameElement = document.createElement("h3");
     nameElement.className = "pc-peer-name";
     nameElement.innerText = name;
+    const bitrateElement = createBitrateInfoElement();
     const buttonsElement = createPeerControlButtonsElement(onStart, onStop);
     element.appendChild(nameElement);
+    element.appendChild(bitrateElement);
     element.appendChild(buttonsElement);
     return element;
 }
@@ -15,13 +17,64 @@ function createPeerControlElement(name, onStart, onStop) {
 function createPeerControlButtonsElement(onStart, onStop) {
     const buttonContainer = document.createElement("div");
     buttonContainer.className = "pc-peer-btn-container";
-    const startButton = createButton("Start", onStart);
+    const startButton = createButton("Start", () => {
+        startButton.classList.add("hidden");
+        stopButton.classList.remove("hidden");
+        onStart();
+    });
     startButton.className = "pc-peer-start-btn";
-    const stopButton = createButton("Stop", onStop);
+    const stopButton = createButton("Stop", () => {
+        startButton.classList.remove("hidden");
+        stopButton.classList.add("hidden");
+        onStop();
+    });
     stopButton.className = "pc-peer-stop-btn hidden";
     buttonContainer.appendChild(startButton);
     buttonContainer.appendChild(stopButton);
     return buttonContainer;
+}
+
+function createPeerElement(name) {
+    const peerElement = document.createElement("div");
+    peerElement.className = "peer";
+    const peerNameElement = document.createElement("p");
+    peerNameElement.innerText = name;
+    peerNameElement.className = "peer-name";
+    const peerStreamElement = document.createElement("video");
+    peerStreamElement.className = "peer-stream hidden";
+    const noStreamElement = document.createElement("p");
+    noStreamElement.className = "peer-no-stream";
+    noStreamElement.innerText = "Not streaming";
+    peerElement.appendChild(peerNameElement);
+    peerElement.appendChild(noStreamElement);
+    peerElement.appendChild(peerStreamElement);
+    peerElement.setStream = (stream) => {
+        peerStreamElement.srcObject = stream;
+        if (stream == null) {
+            noStreamElement.classList.add("hidden");
+            peerStreamElement.classList.remove("hidden");
+        } else {
+            noStreamElement.classList.remove("hidden");
+            peerStreamElement.classList.add("hidden");
+        }
+    }
+    return peerElement;
+}
+
+function createBitrateInfoElement() {
+    const bitrateInfoElement = document.createElement("p");
+    bitrateInfoElement.className = "bitrate";
+    const bitrateElement = document.createElement("span");
+    bitrateElement.className = "bitrate-value";
+    bitrateElement.innerText = -1;
+    const bitrateSuffix = document.createElement("span");
+    bitrateSuffix.innerText = " Kbps";
+    bitrateInfoElement.appendChild(bitrateElement);
+    bitrateInfoElement.appendChild(bitrateSuffix);
+    bitrateInfoElement.setBitrate = (bitrate) => {
+        bitrateElement.innerText = bitrate;
+    }
+    return bitrateInfoElement;
 }
 
 function createButton(content, onClick) {
